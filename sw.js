130eee92b584c234b34ccf8c997a440fa0d79aed
abyk4329 <kseniachud@gmail.com>
2025-08-27 13:50:22 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+// Minimal service worker: cache the app shell (optional)
+const CACHE_NAME = 'money-code-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keyList) => Promise.all(
+      keyList.map((key) => { if (key !== CACHE_NAME) return caches.delete(key); })
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((resp) => resp || fetch(evt.request))
+  );
+});
