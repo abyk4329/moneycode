236e89a75ff28b7259edb790581d43580b168d13
abyk4329 <kseniachud@gmail.com>
2025-08-28 23:07:38 +0300

    Add files via upload

---
+++ sw_new.js
@@ -0,0 +1,43 @@
+// Service Worker for Numerology Calculator PWA
+const CACHE_NAME = 'numerology-calculator-v1';
+const urlsToCache = [
+    '/index_new.html',
+    '/pay_new.html',
+    '/manifest_new.json'
+];
+
+// Install event - cache resources
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch event - serve from cache when offline
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Return cached version or fetch from network
+                return response || fetch(event.request);
+            })
+    );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
